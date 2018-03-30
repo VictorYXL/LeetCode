fca4a5cff4c6017221f42f2b472f7f57b6b4c6f0
Xianliang Yang (MSR Student-FA Talent) <v-xianly@microsoft.com>
2018-03-30 21:35:31 +0800

    No20. Valid Parentheses

---
+++ No20. Valid Parentheses/Solution.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+using namespace std;
+class Solution {
+public:
+	bool static isValid(string s);
+};
---
+++ No20. Valid Parentheses/Source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Solution.h"
+using namespace std;
+int main()
+{
+	cout << Solution::isValid("{}{}{}");
+	return 0;
+}
