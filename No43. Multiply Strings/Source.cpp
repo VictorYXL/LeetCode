8a18469608baf2bf19e9b67b23013c1f483778cf
VictorYXL <hllyq10@163.com>
2018-05-26 23:29:08 +0800

    No43. Multiply Strings

---
+++ No43. Multiply Strings/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Solution.h"
+using namespace std;
+int main()
+{
+    string nums1 = "2", nums2 = "3";
+    cout << Solution::multiply(nums1, nums2).c_str() << endl;
+    return 0;
+}
