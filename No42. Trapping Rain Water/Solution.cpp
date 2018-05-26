3f9f016e203b365dc92122668f304eb8214e95a0
VictorYXL <hllyq10@163.com>
2018-05-26 16:56:00 +0800

    Refine No42. Trapping Rain Water

--- No42. Trapping Rain Water/Solution.cpp
+++ No42. Trapping Rain Water/Solution.cpp
@@ -3,26 +3,22 @@ using namespace std;
 
 int Solution::trap(vector<int>& height)
 {
+    if (height.size() <= 1)
+    {
+        return 0;
+    }
     int sum = 0;
     vector<int> left(height.size(), 0);
     vector<int> right(height.size(), 0);
-
-    for (int i = 0; i < height.size(); i++)
+    left[0] = height[0];
+    right[height.size() - 1] = height[height.size() - 1];
+    for (int i = 1; i < height.size(); i++)
     {
-        for (int j = 0; j < i; j++)
-        {
-            if (right[j] < height[i])
-            {
-                right[j] = height[i];
-            }
-        }
-        for (int j = i + 1; j < height.size(); j++)
-        {
-            if (left[j] < height[i])
-            {
-                left[j] = height[i];
-            }
-        }
+        left[i] = height[i] < left[i - 1] ? left[i - 1] : height[i];
+    }
+    for (int i = height.size() - 2; i >= 0; i--)
+    {
+        right[i] = height[i] < right[i + 1] ? right[i + 1] : height[i];
     }
     for (int i = 0; i < height.size(); i++)
     {
