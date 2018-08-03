2f7b426dbea9fb06a882df56adb921161a0de677
Xianliang Yang <v-xianly@microsoft.com>
2018-08-03 21:02:29 +0800

    No60. Permutation Sequence

---
+++ No60. Permutation Sequence/Solution.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Solution 
+{
+public:
+    static string getPermutation(int n, int k);
+};
---
+++ No60. Permutation Sequence/Source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Solution.h"
+using namespace std;
+int main()
+{
+	cout << Solution::getPermutation(4, 9) << endl;
+	return 0;
+}
